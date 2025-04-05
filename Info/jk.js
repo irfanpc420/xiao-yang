dbf1d637f894352f7a1ca003101031ab3a15f516
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 18:29:35 +0800

    Update jk.js

--- Info/jk.js
+++ Info/jk.js
@@ -1,6 +1,19 @@
-// jimin.js
-const jiminData = {
+module.exports = {
     name: "Jimin",
-    description: "A talented artist and performer.",
-    image: "/images/jimin.jpg"
+    imageUrl: "/images/jk.jpg",
+    bio: "Jimin is a South Korean singer, dancer, and member of the global supergroup BTS. Known for his exceptional vocal range, captivating performances, and charismatic personality, Jimin has won the hearts of millions around the world.",
+    birthDate: "October 13, 1995",
+    nationality: "South Korean",
+    genres: ["Pop", "R&B", "Dance"],
+    debut: "2013",
+    socialLinks: {
+        instagram: "https://www.instagram.com/jimin_bts/",
+        twitter: "https://twitter.com/BTS_twt",
+        youtube: "https://www.youtube.com/channel/UCOh3IerRGqQy6gk2VXA4pRA"
+    },
+    achievements: [
+        "Billboard Hot 100 hits",
+        "MAMA Best Dance Performance Award",
+        "Multiple Gold and Platinum certifications"
+    ]
 };
