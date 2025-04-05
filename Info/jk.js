24e3fa11864a6c982e617438baf3a1565c850cb2
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 17:50:11 +0800

    Update jk.js

--- Info/jk.js
+++ Info/jk.js
@@ -1,4 +1,9 @@
-module.exports = {
+// jk.js
+const jkData = {
+    name: "Jungkook",
+    description: "A rising star with a unique voice.",
+    image: "/images/jk.jpg"
+};.exports = {
   name: 'Jungkook',
   bio: 'The golden maknae of BTS, known for his vocals.',
   image: 'jk.jpg',
