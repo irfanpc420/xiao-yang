6ecf5f076c2c2a28caba8b522a888d8c8a35c4c0
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 17:53:39 +0800

    Update jimin.js

--- Info/jimin.js
+++ Info/jimin.js
@@ -2,5 +2,18 @@
 const jiminData = {
     name: "Jimin",
     description: "A talented artist and performer.",
-    image: "/images/jimin.jpg"
+    image: "/images/jimin.jpg",
+    birthdate: "October 13, 1995",
+    profession: "Singer, Songwriter, Dancer, Composer, Producer",
+    socialMedia: {
+        instagram: "https://www.instagram.com/j.m",
+        twitter: "https://twitter.com/jm",
+        youtube: "https://www.youtube.com/@jiminstudio"
+    },
+    achievements: [
+        "Debut as a member of BTS in 2013",
+        "Released solo singles like 'Promise' and 'Filter'",
+        "Collaborated with various global artists",
+        "Recognized for his unique vocal tone and stage presence"
+    ]
 };
