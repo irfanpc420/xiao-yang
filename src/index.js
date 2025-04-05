273c720dcfd7279cb161b4a0ee2ed1ccd76b341d
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 17:01:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ const actorsRoute = require('./routes/actors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, '../public')));
 
 // API routes for actors
 app.use('/api/actors', actorsRoute);
+
+// Prevent access to source code folders
+app.use('/src', (req, res) => {
+  res.status(403).send('Access to source code is forbidden.');
+});
+app.use('/Info', (req, res) => {
+  res.status(403).send('Access to source code is forbidden.');
+});
 
 // Start the server
 app.listen(port, () => {
