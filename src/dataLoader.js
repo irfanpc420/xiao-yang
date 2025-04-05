858e2fc1cf3261d2eee248462e261893df83a345
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 18:18:28 +0800

    Update dataLoader.js

--- src/dataLoader.js
+++ src/dataLoader.js
@@ -1,15 +1,15 @@
-// Data loader for actors
 const fs = require('fs');
 const path = require('path');
 
+// Function to load all actor data from the Info folder
 function loadActors() {
-  const infoDir = path.join(__dirname, '../Info'); // Path to the Info folder
-  const actors = {};
+  const actorsDir = path.join(__dirname, '../Info');
+  const actorFiles = fs.readdirSync(actorsDir);
+  const actors = [];
 
-  // Read all files in the Info folder
-  fs.readdirSync(infoDir).forEach(file => {
-    const actorId = file.split('.')[0]; // Remove .js extension
-    actors[actorId] = require(path.join(infoDir, file)); // Load the actor data
+  actorFiles.forEach(file => {
+    const actor = require(path.join(actorsDir, file));
+    actors.push(actor);
   });
 
   return actors;
