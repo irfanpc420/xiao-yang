a2ffb9f38181cebf9383dd74cc30ca2089a0386e
irfan420x <marufmdirfanahmed@gmail.com>
2025-04-05 18:19:00 +0800

    Update actors.js

--- src/routes/actors.js
+++ src/routes/actors.js
@@ -1,31 +1,11 @@
-// API routes for actors
 const express = require('express');
 const router = express.Router();
 const dataLoader = require('../dataLoader');
 
-// Load all actors dynamically
-let actors = dataLoader.loadActors();
-
-// API endpoint to get all actors
+// Get list of all actors
 router.get('/', (req, res) => {
-  const actorList = Object.keys(actors).map(id => ({
-    id,
-    name: actors[id].name,
-  }));
-  res.json(actorList);
-});
-
-// API endpoint to get actor details by ID
-router.get('/:id', (req, res) => {
-  const actorId = req.params.id;
-  if (actors[actorId]) {
-    res.json({
-      ...actors[actorId],
-      image: `/images/${actors[actorId].image}`, // Add image path
-    });
-  } else {
-    res.status(404).json({ error: 'Actor not found' });
-  }
+  const actors = dataLoader.loadActors();
+  res.json(actors);
 });
 
 module.exports = router;
